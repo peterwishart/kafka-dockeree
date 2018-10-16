da9f19580585298038a15295c85ed8f4043e99be
Peter Wishart <peter.wishart@zonal.co.uk>
2018-10-16 12:15:04 +0100

    Fix command line for build with docker-ce

--- test.bat
+++ test.bat
@@ -7,9 +7,14 @@ for /f "tokens=1" %%i in ('docker ps -a --filter "name=%appname%*"') do docker r
 
 if [%1]==[kill] goto :skiprun
 
+rem on docker CE, ensure the VM used has enough memory for the build (maybe not needed when running)
+set ce_memopt=
+docker version |findstr /C:"-ce" >nul:
+if errorlevel 0 set ce_memopt=-m 2GB
+
 echo *** Rebuild containers - first run takes a long time ***
-docker build -t %appname%_server .
-docker run -d -p 9000:9000 -p 9092:9092 --name %appname%_server --hostname %appname%_server %appname%_server
+docker build %ce_memopt% -t %appname%_server .
+docker run %ce_memopt% -d -p 9000:9000 -p 9092:9092 --name %appname%_server --hostname %appname%_server %appname%_server
 
 echo ***
 echo kafka running %appname%_server:9092
